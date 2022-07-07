e60b6b926a513c35965c86ff5ae5582f29232c62
SantiagoEnzo <43079878+SantiagoEnzo@users.noreply.github.com>
2022-07-07 00:59:16 -0300

    cleanup,fix defaults

--- santimusica.js
+++ santimusica.js
@@ -7,12 +7,12 @@ const config = require("config");
 let volBajo = config.has('volBajo') ? config.get('volBajo') : 10;
 let volAlto = config.has('volAlto') ? config.get('volAlto') : 45;
 let mensajeBajar = config.has('mensajeBajar') ? config.get('mensajeBajar') : 'Bajando';
-let mensajeSubir = config.has('mensajeSubir') ? config.get('volAlto') : 45;
+let mensajeSubir = config.has('mensajeSubir') ? config.get('volAlto') : "Subiendoo";
 let port = config.has('port') ? config.get('port') : 2592;
+
 //metodos
 app.get('/bajar', async function (req, res) {
   await loudness.setVolume(10);
-  //res.send('Bajado');
   res.send(mensajeBajar);
 })
 
@@ -23,10 +23,10 @@ app.get('/subir', async function (req, res) {
 
 app.get('/volumen', async function (req, res) {	
   let vol = await loudness.getVolume()
-  res.send('Volumen actual: '+vol);
+  res.send('Volumen actual: '+ vol);
 })
 
-//
 
-app.listen(2592);
+//
+app.listen(port);
 console.log('Servidor escuchando en puerto: ' + port);
